10f2fed8fe678d46bc47adf0e33470cea90241ac
daniel garcia <dgarcia@position.cl>
2020-02-26 17:02:01 -0300

    change name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firebase-fileupload-app'`, () => {
+  it(`should have as title 'dinamic-graph-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('firebase-fileupload-app');
+    expect(app.title).toEqual('dinamic-graph-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('firebase-fileupload-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('dinamic-graph-app app is running!');
   });
 });
