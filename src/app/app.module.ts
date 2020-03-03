4ca62f3b790c5720203775b0b8436ea1c6b5a33b
daniel garcia <dgarcia@position.cl>
2020-03-03 10:17:47 -0300

    angular graphics done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    DonaComponent,
+    LineaComponent,
+    RadarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
